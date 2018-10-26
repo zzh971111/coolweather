cd7a905fed965eeb4bd0adbed266f1b169ac1475
zzh971111 <649142242@qq.com>
2018-10-26 11:20:13 +0800

    add  AutoUpdateWeather

--- app/src/main/java/com/coolweather/app/activity/WeatherActivity.java
+++ app/src/main/java/com/coolweather/app/activity/WeatherActivity.java
@@ -13,6 +13,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.coolweather.app.R;
+import com.coolweather.app.service.AutoUpdateService;
 import com.coolweather.app.util.HttpCallbackListener;
 import com.coolweather.app.util.HttpUtil;
 import com.coolweather.app.util.Utility;
@@ -177,6 +178,8 @@ public class WeatherActivity extends Activity implements View.OnClickListener {
         currentDateText.setText(prefs.getString("current_date",""));
         weatherInfoLayout.setVisibility(View.VISIBLE);
         cityNameText.setVisibility(View.VISIBLE);
+        Intent  intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
 
     }
 }
